4056a48b321724c0019315da111606d0834f55a7
David Romero <d.romero@sapalomera.cat>
2025-02-19 15:40:44 +0100

    pasar los datos del server al cliente

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -182,12 +182,24 @@ app.post("/configurar", (req, res) => {
 
   // Enviar configuración a todos los clientes WebSocket
   wsServer.clients.forEach(client => {
-      if (client.readyState === 1) { // Verifica si el cliente está abierto
-          client.send(JSON.stringify({ type: "configuració", ...configuracionJoc }));
-      }
+    if (client.readyState === 1) { // Verifica si el cliente está abierto
+      client.send(JSON.stringify({ type: "configuració", ...configuracionJoc }));
+    }
   });
 
   res.json({ message: "Configuració rebuda!", configuracionJoc });
+});
+
+app.post('/joc', (req, res) => {
+  let joc = req.body.action;
+
+  if (joc == "engegar") {
+    broadcast(JSON.stringify({ type: "estat_joc", running: true }));
+  } else if (joc == "aturar") {
+    broadcast(JSON.stringify({ type: "estat_joc", running: false }));
+  } else {
+    return res.status(400).json({ error: "Acció no vàlida." });
+  };
 });
 
 // Iniciar el servidor
